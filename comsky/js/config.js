e4daac384311bad8939072bd7be797e28c809235
glennpoyogao <114276953+glennpoyogao@users.noreply.github.com>
2023-01-04 10:33:28 +0800

    Add files via upload

---
+++ comsky/js/config.js
@@ -0,0 +1,2 @@
+const MY_API_KEY = '5a1c6c22fcmsh158205c054eea43p14ef4bjsn58666b26f737';
+export { MY_API_KEY };
---
+++ comsky/js/main.js
@@ -0,0 +1,19 @@
+import { MY_API_KEY } from './config.js';
+
+fetch("https://quotes15.p.rapidapi.com/quotes/random/?language_code=en", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "quotes15.p.rapidapi.com",
+		"x-rapidapi-key": "5a1c6c22fcmsh158205c054eea43p14ef4bjsn58666b26f737"
+	}
+})
+.then(response => response.json())
+.then(response => {
+    console.log(response);
+    
+    document.getElementById('quote').innerHTML = response.content;
+    document.getElementById('author').innerHTML = '- ' + response.originator.name + ' -';
+})
+.catch(err => {
+	console.log(err);
+});
